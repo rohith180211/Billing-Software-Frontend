e91f6d5b4b90a02cfd9f4d258887380be23ebefe
Rohith Janwadkar <rohithjanwadkar@Rohiths-MacBook-Air.local>
2025-11-10 13:44:14 -0600

    Added display items

--- src/Components/DisplayItems/DisplayItems.jsx
+++ src/Components/DisplayItems/DisplayItems.jsx
@@ -1,7 +1,24 @@
 import './DisplayItems.css'
+import {useContext} from "react";
+import {AppContext} from "../../context/AppContext.js";
+import Item from "../Item/Item.jsx";
 const DisplayItems = () => {
+    const {itemsData}=useContext(AppContext);
     return (
-        <div className="DisplayItems"></div>
+        <div className="pd-3">
+            <div className="row g-3">
+                {itemsData.map((item, index) => (
+                    <div key={index} className="col-md-4 col-sm-6">
+                        <Item
+                        itemName={item.name}
+                        itemPrice={item.price}
+                         itemImage={item.imgUrl}
+                        itemId={item.id}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 export default DisplayItems;
---
+++ src/Components/Item/Item.jsx
@@ -0,0 +1,21 @@
+import './Item.css'
+const Item = ({itemName,itemPrice,itemImage,itemId}) => {
+    return (
+        <div className="p-3 bg-dark rounded shadow-small h-100 d-flex align-items-center item-card">
+            <div style={{position : "relative" , marginRight:"15px"}}>
+                <img src={itemImage} alt={itemName} className="item-image"/>
+            </div>
+            <div className="flex-grow-1 ms-2">
+                <h6 className="mb-1 text-light">{itemName}</h6>
+                <p className="mb-0 text-light fw-bold">${itemPrice}</p>
+            </div>
+            <div className="d-flex flex-column justify-content-between align-items-center ms-3 " style={{height:'100%'}}>
+                <i className="bi bi-cart-plus fs-4 text-warning"></i>
+                <button className="btn btn-success btn-sm" onClick={hangleAddToCart}>
+                    <i className="bi bi-plus"></i>
+                </button>
+            </div>
+        </div>
+    )
+}
+export default Item;
